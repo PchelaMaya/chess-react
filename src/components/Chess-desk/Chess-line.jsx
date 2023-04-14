1fd540aff707824040ddce1768d3ad9592a21fe7
Maya <pchelaamaya@gmail.com>
2023-04-14 15:27:16 +0300

    add component chess-desk

---
+++ src/components/Chess-desk/Chess-line.jsx
@@ -0,0 +1,22 @@
+const ChessLine = () => {
+    return (<div className="chess-tr">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>	 
+    )
+}
+export {ChessLine}
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,25 @@
 import './Main.css'
 import { Header } from "../Header/Header"
+import { ChessDesk } from '../Chess-desk/Chess-desk'
 
 const Main = () => {
     return (
         <div className="main">
             <div className="main-content w-inner">
                 <Header/>
+                <div className='main-club'>
+                    <h3>Клуб</h3>
+                </div>
+                <div className='main-text'>
+                    <h2 className='main-text__title'>Правильные шахматы</h2>
+                    <p className='main-text__description'>
+                        Приходите к нам, играть в шахматы! Сразитесь 
+                        классическом поединке, блиц состязании
+                        и бою с гройсмейстером.
+                    </p>
+                </div>
+                <ChessDesk/>
+
             </div>
         </div>
 
